c104e517379ffebf1f17708ec6feac6fb7c7aac7
CodePeaceful <christian.schlecht@arcor.de>
2025-12-01 06:30:39 +0100

    day1

--- day1/Task2.cpp
+++ day1/Task2.cpp
@@ -6,16 +6,20 @@ int main() {
     int dial = 50;
     for (const auto& s : v) {
         int change = std::stoi(s.substr(1));
+        dial %= 100;
+        dial += 100;
+        dial %= 100;
         if (s[0] == 'L') {
+            if (dial == 0)
+                --counter;
             dial -= change;
+            if (dial <= 0)
+                counter++;
         }
         else {
             dial += change;
         }
-        dial %= 100;
-        if (dial == 0) {
-            ++counter;
-        }
+        counter += std::abs(dial / 100);
     }
     std::println("{}", counter);
 }
